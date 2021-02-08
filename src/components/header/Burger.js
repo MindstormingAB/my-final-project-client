7cd8a2392b3dc4e0e9731acce9ad7ff826b37039
MindstormingAB <s.elander@mindstorming.se>
2021-02-08 22:11:06 +0100

    Tentative with body-scroll-lock

--- src/components/header/Burger.js
+++ src/components/header/Burger.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 
 import { StyledBurger } from "../../lib/Styling";
 
 const Burger = ({ open, setOpen }) => {
   return (
-    <StyledBurger open={open} onClick={() => setOpen(!open)}>
+    <StyledBurger open={open} onClick={() => {
+      if (open) {
+        setOpen(!open);
+        enableBodyScroll();
+      } else {
+        setOpen(!open);
+        disableBodyScroll();
+      }
+    }
+    }>
       <div />
       <div />
       <div />
